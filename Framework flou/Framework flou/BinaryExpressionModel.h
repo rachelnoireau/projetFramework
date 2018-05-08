5f25acef5a1c5e6be2c7d425b7d7412d6f475798
rachelnoireau <rachel.noireau@uha.fr>
2018-05-08 13:38:16 +0200

    binary expre

--- Framework flou/Framework flou/BinaryExpressionModel.h
+++ Framework flou/Framework flou/BinaryExpressionModel.h
@@ -6,26 +6,51 @@
 
 namespace Core {
 	template <class T>
-	class BinaryExpressionModel :: {
-	private:
-		BinaryExpression<T> *op;
-		Expression<T> *left, *right;
+	class BinaryExpressionModel : public BinaryExpression<T> {
 	
 	public:
 		virtual T evaluate() const;
 		virtual T evaluate(Expression<T>*, Expression<T>*) const;
-		virtual T getLeft();
-		virtual T getRight();
+		virtual T getLeft() const;
+		virtual T getRight() const;
+		virtual T getOperator() const;
 
+	private:
+		BinaryExpression<T> *op;
+		Expression<T> *left, *right;
+		
 	};
+
 	template <class T>
-	T BinaryExpressionModel<T>::evaluate() const {
-		if (left == NULL)
-			return LeftExpressionException;
-		if (right == NULL)
-			return RightExpressionException;
+	T BinaryExpressionModel<T>::getLeft() const {
+		return left;
 	}
 
+	template <class T>
+	T BinaryExpressionModel<T>::getRight() const {
+		return right;
+	}
+
+	template <class T>
+	T BinaryExpressionModel<T>::getOperator() const {
+		return op;
+	}
+
+	template <class T>
+	T BinaryExpressionModel<T>::evaluate() const {
+		if (left == NULL)throw NullOperatorException
+			return LeftExpressionException;
+		if (right == NULL)throw NullOperatorException
+			return RightExpressionException;
+		return evaluate(left, right);
+	}
+
+	template <class T>
+	T BinaryExpressionModel<T>::evaluate(Expression<T>* r,Expression<T>* l) const {
+		if (op == NULL)throw NullOperatorException{
+			return op.evaluate(l,r);
+		}
+	}
 }
 
 
