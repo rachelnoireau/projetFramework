90300dee7fe5acbf695e423143c3138b197d55b4
Hugo Asensio <asensio.hugo@gmail.com>
2018-06-06 17:21:21 +0200

    correction namespace sugenothen

--- Framework flou/Framework flou/SugenoThen.h
+++ Framework flou/Framework flou/SugenoThen.h
@@ -1,10 +1,9 @@
-#pragma once
 #ifndef SUGENOTHEN_H
 #define SUGENOTHEN_H
 
 #include "Then.h"
 
-namespace Core {
+namespace Fuzzy {
 	template <class T>
 	class SugenoThen : public Then<T> {
 	public:
@@ -12,23 +11,11 @@ namespace Core {
 		virtual ~SugenoThen() {};
 		virtual T evaluate(Expression<T>*, Expression<T>*) const;
 
-		/*void updatePremiseValue(Expression<T> * p);*/
-
 		virtual T getPremiseValue();
 
 	private:
 		mutable T premiseValue;
 	};
-
-
-
-	
-
-	/*template<class T>
-	void SugenoThen<T>::updatePremiseValue(Expression<T> * p)
-	{
-		premiseValue = p->evaluate();
-	}*/
 
 	template<class T>
 	T SugenoThen<T>::getPremiseValue()
@@ -41,12 +28,8 @@ namespace Core {
 	template <class T>
 	T SugenoThen<T>::evaluate(Core::Expression<T>* l, Core::Expression<T>* r) const
 	{
-		/*T lev = l->evaluate();
-		T rev = r->evaluate();
-		premiseValue = lev * rev;
-		return premiseValue;*/
-		premiseValue = l->Evaluate();
-		return premiseValue * r->Evaluate();
+		premiseValue = l->evaluate();
+		return premiseValue * r->evaluate();
 	}
 
 }
