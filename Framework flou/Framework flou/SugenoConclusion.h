fa3c4fd764373d4f450bc08ce689e37cec611f98
rachelnoireau <rachel.noireau@uha.fr>
2018-06-06 17:56:16 +0200

    debug

--- Framework flou/Framework flou/SugenoConclusion.h
+++ Framework flou/Framework flou/SugenoConclusion.h
@@ -22,7 +22,7 @@ namespace Fuzzy {
 
 
 	template <class T>
-	T SugenoConclusion<T>::evaluate(std::vector<Expression<T>*>* operands) const
+	T SugenoConclusion<T>::evaluate(std::vector<const Expression<T>*>* operands) const
 	{
 		std::vector<T>::const_iterator itcoef = coeff->begin();
 		std::vector<Core::Expression<T>*>::const_iterator itexpr = operands->begin();
